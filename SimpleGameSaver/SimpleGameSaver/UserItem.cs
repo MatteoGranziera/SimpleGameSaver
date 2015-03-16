003359284b73e14cfc8cb866c4b59c584406d568
Matteo Granziera <granziera-matteo@gmail.com>
2015-03-16 19:59:27 +0100

    Added UserItem class

---
+++ SimpleGameSaver/SimpleGameSaver/UserItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGameSaver
+{
+    public class UserItem
+    {
+        public String Name { get; set; }
+        public List<GameItem> Games { get; set; }
+        public UserItem()
+        {
+
+        }
+
+        public UserItem(String name)
+        {
+            this.Name = name;
+        }
+    }
+}
